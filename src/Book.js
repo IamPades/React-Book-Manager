129290cf83708a06dd34b98da384b5365a3ea1a3
Paul <pauldesir7@gmail.com>
2023-12-07 17:44:18 -0500

    Part 1 Done

---
+++ src/Book.js
@@ -0,0 +1,49 @@
+class Book {
+  static bookCount = 0; // Static property to keep track of book count
+
+  // Constructor with default values
+  constructor(title = '', author = '', isbn = '', price = 0.0) {
+    this._attributes = {
+      title,
+      author,
+      isbn,
+      price
+    };
+    Book.bookCount++; // Increment book count on each new instance
+  }
+
+  // Display attribute values
+  getAttribute(attribute) {
+    if (this._attributes.hasOwnProperty(attribute)) {
+      return this._attributes[attribute];
+    }
+    throw new Error(`Attribute ${attribute} not found`);
+  }
+
+  // Modify attribute values
+  setAttribute(attribute, value) {
+    if (this._attributes.hasOwnProperty(attribute)) {
+      this._attributes[attribute] = value;
+    } else {
+      throw new Error(`Attribute ${attribute} not found`);
+    }
+  }
+
+  // Counts of created books
+  static getNumberOfCreatedBooks() {
+    return Book.bookCount;
+  }
+
+  // Compare two books for equality based on ISBN & price
+  equals(otherBook) {
+    return this._attributes.isbn === otherBook.getAttribute('isbn') && 
+           this._attributes.price === otherBook.getAttribute('price');
+  }
+
+  // Method to display book information using console.log
+  displayInfo() {
+    console.log(`Title: ${this._attributes.title}, Author: ${this._attributes.author}, ISBN: ${this._attributes.isbn}, Price: ${this._attributes.price}`);
+  }
+}
+
+export default Book;
